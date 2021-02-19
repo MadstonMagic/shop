9a680733febb4fc9e8eef3213add15f8fb3d47db
MadstonMagic <sapphirecraft.help@gmail.com>
2021-02-19 15:22:41 +0100

    First Initial Release
    Shop - MadstonMagic

---
+++ js/posts-filter-v1.0.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+	//-------------------
+	//Show all Categories
+	//-------------------
+	$('.cat_all').on('click',function() {
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_all').css("color", "var(--white)");
+		$('.single-post.all').css("display","block");
+	});
+	
+	//-------------------
+	//Show Firearms Only
+	//-------------------
+	$('.cat_firearms').on('click',function() {
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_firearms').css("color", "var(--white)");
+		$('.single-post.all').css("display","none");
+		$('.single-post.firearms').css("display","block");
+	});
+	
+	//-------------------
+	//Show Weaponry Only
+	//-------------------
+	$('.cat_weaponry').on('click',function() {
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_weaponry').css("color", "var(--white)");
+		$('.single-post.all').css("display","none");
+		$('.single-post.weaponry').css("display","block");
+	});
+	
+	//-------------------
+	//Show Creatures Only
+	//-------------------
+	$('.cat_creatures').on('click',function() {
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_creatures').css("color", "var(--white)");
+		$('.single-post.all').css("display","none");
+		$('.single-post.creatures').css("display","block");
+	});
+	
+	//-------------------
+	//Show Misc Only
+	//-------------------
+	$('.cat_misc').on('click',function() {
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_misc').css("color", "var(--white)");
+		$('.single-post.all').css("display","none");
+		$('.single-post.creatures').css("display","block");
+	});
+	
+});
---
+++ js/script.js
@@ -0,0 +1,136 @@
+$(document).ready( function() {
+	
+	/********************/
+	/* Back Home Button */
+	/********************/
+	
+	$('.back_to_home').on('click', function() {
+		goBack();
+	});
+	
+	/* =========================== */
+	/*= Disable Accidental CTRL+A =*/
+	/* =========================== */	
+	document.body.addEventListener('keydown', event => {
+		if (event.ctrlKey && 'asp'.indexOf(event.key) !== -1) {
+			event.preventDefault();
+		}
+	});
+	//-----------------------------------------------------------------------------------
+		
+	
+	/* ======================== */
+	/* ======= Subpages ======= */
+	/* ======================== */		
+	
+	// Pictures on the sub-pages
+	//$('.posts-collection .post-img .content-img').on('click', function() {
+	$('.content-img').on('click', function() {
+		var source = $(this).attr('src');
+		
+		$('#imgLarge img').attr('src', source);
+		$('#imgLarge').addClass('visible');
+		$('#imgLarge').removeClass('hidden');
+		
+		$('#imgLarge').fadeIn(300);
+		
+		
+		$('#imgLarge .background').on('click', function (){
+			closeImg();			
+		});
+		
+		$('#imgLarge .exit').on('click', function() {
+			closeImg();
+		});
+		
+		function closeImg() {
+			$('#imgLarge').removeClass('visible');
+			$('#imgLarge').addClass('hidden');
+			$('#imgLarge').fadeOut(300);			
+		}
+	});	
+	//-----------------------------------------------------------------------------------
+	
+	
+	/* ========================= */
+	/* ===== To Top Button ===== */
+	/* ========================= */	
+	$('.toTopWrapper').css("display","none");
+	$('.toTopWrapper i').on('click', function() {
+		window.scrollTo(0,0);	
+	});
+	
+	$(document).scroll(function() {
+		var y = $(this).scrollTop();
+		
+		if (y > 800) {
+			$('.toTopWrapper').fadeIn(200);
+		} else {
+			$('.toTopWrapper').fadeOut(200);
+		}
+	});
+	//-----------------------------------------------------------------------------------
+	
+	
+	/* ======================= */
+	/* ===== Sticky Menu ===== */
+	/* ======================= */	
+	$(document).scroll(function() {
+		var y = $(this).scrollTop();
+		
+		if (y > 200) {
+			$('.stickyMenuWrapper').addClass("visible");
+		} else if(y < 200) {
+			$('.stickyMenuWrapper').removeClass("visible");
+		}
+	});	
+	//-----------------------------------------------------------------------------------
+	
+	
+	
+	/* ==================== */
+	/* ===== Contents ===== */
+	/* ==================== */
+	$('.contents-click').on('click', function() {
+		$(this).find('ul').toggleClass('visibleContents');
+	});
+	
+	
+	
+	
+	
+	$(function () {
+		count = 0;
+		textArray = [
+			'Madston\'s Magic',
+			'Tip: Click on the images to enlarge them.',
+			'3D modeler of Minecraft Java models',
+			'Fulltime PC user',
+			'Addicted to looking at pictures of cool games I can\'t afford',
+			'Professional Gaming Chair user.'
+		];
+		
+		setInterval(function () {
+			count++;
+			$(".header .slider_text").fadeOut(300, function () {
+				$(this).html(textArray[count % textArray.length]).fadeIn(300);
+			});
+		}, 4000);
+	});	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
